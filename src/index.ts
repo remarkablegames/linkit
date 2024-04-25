d824680fc76da7aa08832b4482f5ecffc0b11bc6
Mark <mark@remarkablemark.org>
2024-04-24 21:10:07 -0400

    refactor(index): tidy game config and add title, url, version

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import Phaser from 'phaser';
 
-import { Boot, Main } from './scenes';
+import * as scenes from './scenes';
 
 new Phaser.Game({
   width: 600,
   height: 800,
-  scene: [Boot, Main],
+  title: 'Linkit',
+  url: import.meta.env.VITE_APP_HOMEPAGE,
+  version: import.meta.env.VITE_APP_VERSION,
+  scene: [
+    scenes.Boot,
+    ...Object.values(scenes).filter((scene) => scene !== scenes.Boot),
+  ],
   transparent: true,
   scale: {
     mode: window.innerWidth < 600 ? Phaser.Scale.FIT : Phaser.Scale.NONE,
